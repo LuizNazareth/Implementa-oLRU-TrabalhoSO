04263ffa4f27f0fc8189ef45d68053aa620c35ab
gabrielmattosXXF <gabriel.mattos@estudante.ufjf.br>
2024-08-24 11:44:56 -0300

    Implementacao das classes Lru e paginas

--- lru.py
+++ lru.py
@@ -1,12 +1,32 @@
 from pagina import Pagina
 
-class Lru:
-    def __init__(self, tamanho: int, paginas):
+class Lru: ##classe que representa a memoria fisica
+    def __init__(self, tamanho, paginas):
         self.tamanho = tamanho
-        self.paginas = paginas ##estrutura que traz todas as paginas que estao armazenadas na cache
+        self.paginas = paginas ##estrutura que traz todas as paginas que na memoria fisica
+        # [pag1, pag2, pag3, pag4, pag5]
+        self.lru = range(tamanho) ##estrutura que traz o indice que estao armazenadas na memoria fisica
         
-    def acessar_pagina(self, vector_pag_acessadas):
-        for i in vector_pag_acessadas:
-            
-        
-    
+    def acessar_pagina(self, pag_acessadas):
+        #[1, 0, 0, 1, 1]
+        for idx, pag in enumerate(self.paginas):
+            if(pag_acessadas[idx] == 1):
+                pag.referencia()
+            else:
+                pag.nao_referencia()
+        self.ordena_lru()
+
+                
+    def ordena_lru(self):
+        print("Ordenando paginas")
+        ##percorrer as paginas e ordená a lista lru de acordo com os pesos das paginas
+
+        self.lru = sorted(self.lru, key=lambda x: self.paginas[x].peso)
+
+
+    def adiciona_pagina(self, pagina):
+        idx_remove = self.lru[-1]
+        pagina_removida = self.paginas[idx_remove]
+        self.paginas[idx_remove] = pagina
+        pagina.referencia()
+        self.ordena_lru()
